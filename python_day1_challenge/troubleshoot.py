ee02f316c3eafcbe8f6180a7a6d6c43b74e51489
leontronics <alberto.leon@icloud.com>
2023-10-30 21:06:42 +0000

    added extra challenge

---
+++ python_day1_challenge/troubleshoot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def main():
+    user_input = input("Enter some words separated by spaces: ")
+    words = user_input.split(" ")
+
+    print("Splitting the words:")
+    print(words)
+
+    joined_words = "-".join(words)
+    print("Joining the words with hyphens:")
+    print(joined_words)
+
+main()
