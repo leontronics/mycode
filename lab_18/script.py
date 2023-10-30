c9eb2ce6427bf150285973c2102ce8e3bf1c85c7
leontronics <alberto.leon@icloud.com>
2023-10-30 19:56:47 +0000

    added lab 18

---
+++ lab_18/script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+
+    name_input = input ("Please enter your name: ")
+    dayOfWeek_input = input ("What is the current day of the week: ")
+
+    print("Hello " + name_input + " Happy " + dayOfWeek_input + "!")
+
+main()
