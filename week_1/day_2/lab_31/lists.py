bc7ecadf7919fb27bfd4fcedf069da592b827413
leontronics <alberto.leon@icloud.com>
2023-10-31 20:04:45 +0000

    cleaned up

--- week_1/day_2/lab_31/lists.py
+++ week_1/day_2/lab_31/lists.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 import random
-def main():
 
-    wordbank= ["indentation", "spaces"] 
-    tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+def getStudentIndex(max_index):
+    while True:
+        user_input = input(f"Please enter a number between 0 and {max_index} or type 'random': ").strip().lower()
+        
+        if user_input == "random":
+            return random.randint(0, max_index)
+        
+        try:
+            index = int(user_input)
+            if 0 <= index <= max_index:
+                return index
+            else:
+                raise ValueError
+        except ValueError:
+            retry = input("Invalid input. Would you like to retry? (yes/no): ").strip().lower()
+            if retry != 'yes':
+                print("Choosing a random number instead then")
+                return random.randint(0, max_index)
+
+def main():
+    wordbank = ["indentation", "spaces"]
+    tlgstudents = ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
 
     wordbank.append(4)
-    num = int(input(f"Please enter a number between 0 and {len(tlgstudents)-1}: "))
 
-    student_name = tlgstudents[num]
+    max_index = len(tlgstudents) - 1
+    student_index = getStudentIndex(max_index)
 
-    print(f"{student_name} always uses {wordbank[-1]} {wordbank[1]} to indent")
-
-    currentStudents = ["Bert", "Angel", "Chandan", "Chris", "Gen", "Jojo", "Joseph", "Robert", "Sar", "Zack"]
-    randomNum = random.randint(0, (len(currentStudents)-1))
-    otherStudentName = currentStudents[randomNum]
-    print("\nRandom student")
-    print(f"{otherStudentName} always uses {wordbank[-1]} {wordbank[1]} to indent")
-
+    student_name = tlgstudents[student_index]
+    print(f"{student_name} always uses {str(wordbank[-1])} {wordbank[1]} to indent")
 
 if __name__ == "__main__":
     main()
