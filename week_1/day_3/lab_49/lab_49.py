c31abd1fa5cdf749e241bf1af31d23e424621f38
Alberto Leon <alberto.leon@icloud.com>
2023-11-01 11:59:52 -0500

    added lab_49

---
+++ week_1/day_3/lab_49/lab_49.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Number guessing game! User has 5 chances to guess a number between 1 and 100!"""
+import random
+
+def main():
+    num = random.randint(1, 100)
+    rounds = 0
+    guess = None  
+
+    while rounds < 5 and guess != num:
+        guess = input("Guess a number between 1 and 100\n>")
+
+        
+        if guess.isdigit():
+            guess = int(guess)
+        else:
+            print("Please enter a valid number.")
+            continue
+
+        if guess > num:
+            print("Too high!")
+            rounds += 1
+        elif guess < num:
+            print("Too low!")
+            rounds += 1
+        else:
+            print("Correct!")
+            break  
+
+if __name__ == "__main__":
+    main()
