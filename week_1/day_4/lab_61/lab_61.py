17b31d8c706b9dec74f7b148e140e64ba1995864
Alberto Leon <alberto.leon@icloud.com>
2023-11-02 12:05:07 -0500

    added lab_61

---
+++ week_1/day_4/lab_61/lab_61.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def get_max_beers(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < 0:
+                raise ValueError
+            return value
+        except ValueError:
+            print("It has to be a non-negative number. Please try again.")
+
+def sing():
+    max_beers = get_max_beers("Enter the maximum number of beers on the wall: ")
+
+    for num in range(max_beers, -1, -1):
+        if num > 1:
+            print(f'{num} bottles of beer on the wall, {num} bottles of beer.')
+            print(f'Take one down and pass it around, {num - 1} bottles of beer on the wall.\n')
+        elif num == 1:
+            print(f'{num} bottle of beer on the wall, {num} bottle of beer.')
+            print('Take one down and pass it around, no more bottles of beer on the wall.\n')
+        elif num == 0:
+            print('No more bottles of beer on the wall, no more bottles of beer.')
+            print(f'Go to the store and buy some more, {max_beers} bottles of beer on the wall.')
+
+def main():
+    sing()
+
+
+if __name__ == "__main__":
+    main()
