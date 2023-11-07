d585d59e3ca188d347e5722bd05e364283c2c899
Alberto Leon <alberto.leon@icloud.com>
2023-11-07 10:11:41 -0600

    Pulling in data from API with python standard library

--- iss/ride_iss.py
+++ iss/ride_iss.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Alta3 Research - astros on ISS"""
+"""Alta3 Research - tracking ISS updated output"""
 
 import urllib.request
 import json
@@ -15,42 +15,15 @@ def main():
     # the problem here, is that it will read out as a string
     helmet = groundctrl.read()
 
-    # show that at this point, our data is str
-    # we want to convert this to list / dict
-    print(helmet)
-
     helmetson = json.loads(helmet.decode("utf-8"))
 
-    # this should say bytes
-    print(type(helmet))
-
-    # this should say dict
-    print(type(helmetson))
-
-    print(helmetson["number"])
-
-    # this returns a LIST of the people on this ISS
-    print(helmetson["people"])
-
-    # list the FIRST astro in the list
-    print(helmetson["people"][0])
-
-    # list the SECOND astro in the list
-    print(helmetson["people"][1])
-
-    # list the LAST astro in the list
-    print(helmetson["people"][-1])
-
-    # display every item in a list
-    for astro in helmetson["people"]:
-        # display what astro is
-        print(astro)
+    # display people in space
+    print("People in space: " + str(helmetson["number"]))
 
     # display every item in a list
     for astro in helmetson["people"]:
         # display ONLY the name value associated with astro
-        print(astro["name"])
+        print(astro["name"] + " on the " + astro["craft"])
 
 if __name__ == "__main__":
     main()
-
