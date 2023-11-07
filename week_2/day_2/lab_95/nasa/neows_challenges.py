daaaab4528de1292b2a72c35058a25f55fbc0138
Alberto Leon <alberto.leon@icloud.com>
2023-11-07 14:17:50 -0600

    added lab95

--- week_2/day_2/lab_95/nasa/neows_challenges.py
+++ week_2/day_2/lab_95/nasa/neows_challenges.py
@@ -3,54 +3,65 @@ import requests
 from dotenv import load_dotenv
 import os
 
-# Define NEOW URL
-NEOURL = "https://api.nasa.gov/neo/rest/v1/feed?"
-
 # Load environment variables from .env file
 load_dotenv()
 
-def get_api_key():
-    """
-    Retrieves NASA API key from environment variables.
-    """
-    return os.getenv('NASA_API_KEY')
+NASA_API_KEY = os.getenv('NASA_API_KEY')
+NEO_FEED_URL = 'https://api.nasa.gov/neo/rest/v1/feed?'
 
-def build_api_url(start_date, end_date=None):
-    """
-    Constructs the API URL with the given start date and optional end date.
-    """
-    api_key = get_api_key()
-    url = f"{NEOURL}start_date={start_date}"
-    if end_date:
-        url += f"&end_date={end_date}"
-    url += f"&api_key={api_key}"
-    return url
 
-def get_neo_data(api_url):
-    """
-    Makes a GET request to the NASA API and returns the data.
-    """
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-        return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+def get_asteroid_data(start_date, end_date):
+    #Fetches asteroid data from NASA's API within the specified date range.
+    params = {
+        'start_date': start_date,
+        'end_date': end_date,
+        'api_key': NASA_API_KEY
+    }
+    response = requests.get(NEO_FEED_URL, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def find_extreme_asteroids(asteroid_data):
+    #Finds the largest, fastest, and closest asteroids from the dataset.
+    largest = {'name': '', 'diameter': 0}
+    fastest = {'name': '', 'speed': 0}
+    closest = {'name': '', 'distance': float('inf')}
+
+    for daily_asteroids in asteroid_data.values():
+        for asteroid in daily_asteroids:
+            diameter = asteroid['estimated_diameter']['meters']['estimated_diameter_max']
+            speed = float(asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'])
+            distance = float(asteroid['close_approach_data'][0]['miss_distance']['kilometers'])
+
+            if diameter > largest['diameter']:
+                largest.update({'name': asteroid['name'], 'diameter': diameter})
+            if speed > fastest['speed']:
+                fastest.update({'name': asteroid['name'], 'speed': speed})
+            if distance < closest['distance']:
+                closest.update({'name': asteroid['name'], 'distance': distance})
+
+    return largest, fastest, closest
+
 
 def main():
-    """
-    Main function to handle user input and display NEO data.
-    """
-    start_date = input("Enter the start date (YYYY-MM-DD): ")
-    end_date = input("Enter the end date (YYYY-MM-DD) or press enter to skip: ")
+    try:
+        start_date = input('Enter the start date (YYYY-MM-DD): ')
+        end_date = input('Enter the end date (YYYY-MM-DD) or press enter to skip: ') or start_date
 
-    api_url = build_api_url(start_date, end_date)
-    neo_data = get_neo_data(api_url)
+        asteroid_data = get_asteroid_data(start_date, end_date)['near_earth_objects']
+        largest, fastest, closest = find_extreme_asteroids(asteroid_data)
 
-    if neo_data:
-        print(neo_data)
+        print(f'The largest asteroid is {largest["name"]} with a diameter of {largest["diameter"]} meters.')
+        print(f'The fastest asteroid is {fastest["name"]} with a speed of {fastest["speed"]} kilometers per hour.')
+        print(f'The closest asteroid is {closest["name"]} with a distance of {closest["distance"]} kilometers.')
+    except requests.RequestException as e:
+        print(f'Error fetching data from NASA API: {e}')
+    except ValueError as e:
+        print(f'Invalid date format: {e}')
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
